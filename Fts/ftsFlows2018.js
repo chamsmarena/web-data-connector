b7e3e09a069d128737f88bd1b22919296b0e3f1e
chamsmarena <chamsmarena@hotmail.com>
2018-04-27 11:16:15 +0000

    add new files

--- Fts/ftsFlows2018.js
+++ Fts/ftsFlows2018.js
@@ -4,27 +4,75 @@
 
     // Define the schema
     myConnector.getSchema = function(schemaCallback) {
-        var cols = [{
-            id: "id",
-            dataType: tableau.dataTypeEnum.string
-        }, {
-            id: "mag",
-            alias: "magnitude",
-            dataType: tableau.dataTypeEnum.float
-        }, {
-            id: "title",
-            alias: "title",
-            dataType: tableau.dataTypeEnum.string
-        }, {
-            id: "location",
-            dataType: tableau.dataTypeEnum.geometry
-        }];
+        var cols = [
+            {
+                id: "id",
+                dataType: tableau.dataTypeEnum.string
+            }, 
+            {
+                id: "description",
+                dataType: tableau.dataTypeEnum.string
+            },
+            {
+                id: "status",
+                dataType: tableau.dataTypeEnum.string
+            },
+            {
+                id: "amountUSD",
+                alias: "amountUSD",
+                dataType: tableau.dataTypeEnum.float
+            },
+            {
+                id: "originalAmount",
+                alias: "originalAmount",
+                dataType: tableau.dataTypeEnum.float
+            },
+            {
+                id: "originalCurrency",
+                alias: "originalCurrency",
+                dataType: tableau.dataTypeEnum.string
+            }, 
+            {
+                id: "exchangeRate",
+                alias: "exchangeRate",
+                dataType: tableau.dataTypeEnum.float
+            },
+            {
+                id: "budgetYear",
+                alias: "budgetYear",
+                dataType: tableau.dataTypeEnum.string
+            },
+            {
+                id: "flowType",
+                alias: "flowType",
+                dataType: tableau.dataTypeEnum.string
+            },
+            {
+                id: "contributionType",
+                alias: "contributionType",
+                dataType: tableau.dataTypeEnum.string
+            },
+            {
+                id: "boundary",
+                alias: "boundary",
+                dataType: tableau.dataTypeEnum.string
+            },
+            {
+                id: "onBoundary",
+                alias: "onBoundary",
+                dataType: tableau.dataTypeEnum.string
+            },
+            {
+                id: "refCode",
+                alias: "refCode",
+                dataType: tableau.dataTypeEnum.string
+            }];
 
-        var tableSchema = {
-            id: "flowFeed",
-            alias: "Flows on FTS",
-            columns: cols
-        };
+            var tableSchema = {
+                id: "flowFeed",
+                alias: "Flows on FTS",
+                columns: cols
+            };
 
         schemaCallback([tableSchema]);
     };
@@ -38,10 +86,19 @@
             // Iterate over the JSON object
             for (var i = 0, len = feat.length; i < len; i++) {
                 tableData.push({
-                    "id": feat[i].description,
-                    "mag": feat[i].amountUSD,
-                    "title": "",
-                    "location": ""
+                    "id": feat[i].id,
+                    "description": feat[i].description,
+                    "status": feat[i].status,
+                    "amountUSD": feat[i].amountUSD,
+                    "originalAmount": feat[i].originalAmount,
+                    "originalCurrency": feat[i].originalCurrency,
+                    "exchangeRate": feat[i].exchangeRate,
+                    "budgetYear": feat[i].budgetYear,
+                    "flowType": feat[i].flowType,
+                    "contributionType": feat[i].contributionType,
+                    "boundary": feat[i].boundary,
+                    "onBoundary": feat[i].onBoundary,
+                    "refCode": feat[i].refCode
                 });
             }
 
