af0dd0424f519aabab58584933222ea823d19299
chamsmarena <chamsmarena@hotmail.com>
2018-04-27 08:15:49 +0000

    fisrt version

---
+++ Fts/ftsFlows2018.js
@@ -0,0 +1,62 @@
+(function() {
+    // Create the connector object
+    var myConnector = tableau.makeConnector();
+
+    // Define the schema
+    myConnector.getSchema = function(schemaCallback) {
+        var cols = [{
+            id: "id",
+            dataType: tableau.dataTypeEnum.string
+        }, {
+            id: "mag",
+            alias: "magnitude",
+            dataType: tableau.dataTypeEnum.float
+        }, {
+            id: "title",
+            alias: "title",
+            dataType: tableau.dataTypeEnum.string
+        }, {
+            id: "location",
+            dataType: tableau.dataTypeEnum.geometry
+        }];
+
+        var tableSchema = {
+            id: "flowFeed",
+            alias: "Flows on FTS",
+            columns: cols
+        };
+
+        schemaCallback([tableSchema]);
+    };
+
+    // Download the data
+    myConnector.getData = function(table, doneCallback) {
+        $.getJSON("https://api.hpc.tools/v1/public/fts/flow?planid=638", function(resp) {
+            var feat = resp.data.flows,
+                tableData = [];
+
+            // Iterate over the JSON object
+            for (var i = 0, len = feat.length; i < len; i++) {
+                tableData.push({
+                    "id": feat[i].description,
+                    "mag": feat[i].amountUSD,
+                    "title": "",
+                    "location": ""
+                });
+            }
+
+            table.appendRows(tableData);
+            doneCallback();
+        });
+    };
+
+    tableau.registerConnector(myConnector);
+
+    // Create event listeners for when the user submits the form
+    $(document).ready(function() {
+        $("#submitButton").click(function() {
+            tableau.connectionName = "FTS Mali 2018"; // This will be the data source name in Tableau
+            tableau.submit(); // This sends the connector object to Tableau
+        });
+    });
+})();
